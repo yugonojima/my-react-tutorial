3fadc03b6046f86cc364682b36e1ea2a39965f54
yugonojima <mmrsbhr19990113@gmail.com>
2020-08-08 13:05:43 +0900

    hello

---
+++ src/Game.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Board from './Board';
+import './Game.css'
+
+class Game extends React.Component {
+  render() {
+    return(
+      <div className="game">
+        <div className="game-board">
+          <Board/>
+        </div>
+        <div className="game-info">
+          <div>{/*status*/}</div>
+          <div>{/*TODO*/}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Game
