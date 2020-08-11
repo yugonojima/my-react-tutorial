f6bf73e39ceb0bbe3e1a1928d7376c0bd64e1fd0
野島佑悟 <yugonojima@nojimayuusatorunoMacBook-Pro.local>
2020-08-11 22:18:48 +0900

    complete

--- src/Board.js
+++ src/Board.js
@@ -3,38 +3,27 @@ import Square from "./Square";
 import "./Board.css";
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
   // stateを更新する関数
   // 直接データを書き換えるのではなく、コピーを作成し、新しいデータで古いデータを上書きしている
-  handleClick(i) {
-    const squares = this.state.squares.slice();
 
-    squares[i] = this.state.xIsNext ? "✖" : "〇";
-    this.setState({ squares: squares , xIsNext: !this.state.xIsNext });
-  }
+
+
 
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const status = 'Next player: '+ (this.state.xIsNext ? '✖︎' :'○');
+
 
     return (
       <div>
-        <div className="status">{status}</div>
+
 
         <div className="board-row">
           {this.renderSquare(0)}
